0664937684046ba5cc53aaeed34a944885535fab
Paul P <fluffycolombian10@gmail.com>
2022-10-04 14:57:11 -0400

    First Commit

--- streamlit.py
+++ streamlit.py
@@ -33,6 +33,6 @@ with header:
 
 with dataset:
     table = pd.DataFrame(rows)
-    fmt = "%d-%m-%Y %H:%M:%S"
+    fmt = "%m-%d-%Y %I:%M %p"
     table.columns = ['Player', 'Team', 'Sport', 'Stat Type', 'Last Updated', 'Line', 'Probability (%)', 'Favor']
     st.dataframe(table.style.highlight_quantile(axis=0, subset='Probability (%)', color='#97F589', q_right=1, q_left=0.8).format({"Line": "{:.1f}", "Probability (%)": "{:.1f}"}).format({"Last Updated": strftime(fmt)}) , use_container_width=True)
