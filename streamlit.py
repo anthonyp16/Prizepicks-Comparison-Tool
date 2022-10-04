42992606a4d0b368a19742b1361eabb15a9d1d8f
Paul P <fluffycolombian10@gmail.com>
2022-10-04 14:56:23 -0400

    First Commit

--- streamlit.py
+++ streamlit.py
@@ -29,7 +29,10 @@ with header:
     st.markdown("Prizepicks has countless sports picks playing at a given time. And you don't need to know much about every sport to use this tool. Lots of the plays on Prizepicks are being offered on other sportsbooks, and we can use that to our advantage. You can go to a sportsbook (Draftkings in this case) and compare the odds. Every play on Prizepicks is **assumed to be 50/50**, but that's impossible for every line.")
     st.markdown("*For example, Lionel Messi at 2 Shots is being offered on Prizepicks as an over/under, but on Draftkings, the over is -150 (60%). This is **not** a 50/50 play, there is a favor towards the over.*")
 
+
+
 with dataset:
     table = pd.DataFrame(rows)
+    fmt = "%d-%m-%Y %H:%M:%S"
     table.columns = ['Player', 'Team', 'Sport', 'Stat Type', 'Last Updated', 'Line', 'Probability (%)', 'Favor']
-    st.dataframe(table.style.highlight_quantile(axis=0, subset='Probability (%)', color='#97F589', q_right=1, q_left=0.8).format({"Line": "{:.1f}", "Probability (%)": "{:.1f}"}).format({"Last Updated": strftime()}) , use_container_width=True)
+    st.dataframe(table.style.highlight_quantile(axis=0, subset='Probability (%)', color='#97F589', q_right=1, q_left=0.8).format({"Line": "{:.1f}", "Probability (%)": "{:.1f}"}).format({"Last Updated": strftime(fmt)}) , use_container_width=True)
