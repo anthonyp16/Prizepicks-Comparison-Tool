0588d8b75a7af8222dc7b52a9187fbcbce70ed33
Paul P <fluffycolombian10@gmail.com>
2022-11-03 12:58:10 -0400

    streamlit app

--- streamlit.py
+++ streamlit.py
@@ -53,7 +53,7 @@ with dataset:
 #     # .apply(highlight2))
 
     st.dataframe(filtered_df.style.highlight_quantile(axis=0, subset='Probability', color='#48F084', q_right=1, q_left=0.8)\
-    .format({"Line": "{:.1f}", "Probability": "{:.1f}%", "Last Updated": lambda x: "{}".format(x.strftime("%m/%d/%y %H:%M"))})\
+    .format({"Over": "{:.0f}","Line": "{:.1f}", "Probability": "{:.1f}%", "Last Updated": lambda x: "{}".format(x.strftime("%m/%d/%y %H:%M"))})\
     .apply(highlight)\
     .apply(highlight2), use_container_width=True)
 
