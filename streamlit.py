9f4accde4d6163375849fef9531eb5d0a732830c
Paul P <fluffycolombian10@gmail.com>
2022-11-13 21:20:56 -0500

    streamlit app

--- streamlit.py
+++ streamlit.py
@@ -4,6 +4,47 @@ import streamlit_analytics
 import pandas as pd
 from gsheetsdb import connect
 
+import pathlib
+from bs4 import BeautifulSoup
+import logging
+import shutil
+
+
+def inject_ga():
+    GA_ID = "google_analytics"
+
+    # Note: Please replace the id from G-XXXXXXXXXX to whatever your
+    # web application's id is. You will find this in your Google Analytics account
+    
+    GA_JS = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8Y0SN4CW1K"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8Y0SN4CW1K');
+</script>
+    """
+
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):  # if cannot find tag
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # recover from backup
+        else:
+            shutil.copy(index_path, bck_index)  # keep a backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+
+inject_ga()
+
 st.set_page_config(page_title="PrizePicks Odds", page_icon=":moneybag:", layout='wide')
 
 conn = connect()
